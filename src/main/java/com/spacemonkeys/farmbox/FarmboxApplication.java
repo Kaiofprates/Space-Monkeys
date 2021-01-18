05a84d305d3998369e32b0ca782e6754f12f49c0
Kaiofprates <kaioprates@outlook.com>
2021-01-18 19:27:02 -0300

    first commit

---
+++ src/main/java/com/spacemonkeys/farmbox/FarmboxApplication.java
@@ -0,0 +1,13 @@
+package com.spacemonkeys.farmbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmboxApplication.class, args);
+	}
+
+}
